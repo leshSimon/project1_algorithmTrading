d4b08eb46e4930c8f1e46e48660fd20b28cf884e
Forceps <54647455+Forceps@users.noreply.github.com>
2021-06-02 19:20:24 +0900

    s
    s

--- train_by_code.py
+++ train_by_code.py
@@ -6,7 +6,6 @@ pymon = PyMon()
 
 pymon.simulationInit(startDate=20190515)
 
-while True:
-    if pymon.mySituation[1] < pymon.today:
-        pymon.simulation_at_one_point(learning=True)
+while pymon.mySituation[1] < pymon.today:
+    pymon.simulation_at_one_point(learning=True)
 
